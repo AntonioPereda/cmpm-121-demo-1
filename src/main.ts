d4b0a15c9c81158f42374a87037e72a9af6604af
anapered <anapered@ucsc.edu>
2024-10-05 13:36:32 -0700

    Changed name of project

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "Antonio's 'Original' Game";
 document.title = gameName;
 
 const header = document.createElement("h1");
