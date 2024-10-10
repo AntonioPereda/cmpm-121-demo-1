2a668171e671fc3111c085e3465609f322186010
AntonioPereda <anapered@ucsc.edu>
2024-10-10 15:04:43 -0700

    STEP 6 DONE!!! WHOAAA-AH WERE HALFWAY THEEEE-EERRREEE

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-This is the starter code project for the `Demo 1` project in CMPM 121, Game Design Patterns, at UC Santa Cruz (fall quarter, 2024). Students should incrementally modify this project, tracking their progress wit git, to develop an incremental game of their own unique design.
+This is the starter code project for the `Demo 1` project in CMPM 121, Game Design Patterns, at UC Santa Cruz (fall quarter, 2024). Students should incrementally modify this project, tracking their progress with git, to develop an incremental game of their own unique design.
 
 ####CHANGELOG####
 
-- !!STEP 5 COMPLETE!!
-- Added helpful comments in main.ts
-- Renamed incrimentCount to globalUpdate
-- Renamed the main button "button" to "theGameButton"
-- Number of clicks now auto-update within globalUpdate
-
-- Implimented AutoClick Upgrades
-    - AutoClick rate changed from 1 to 0 / sec
-    - Added AutoClick upgrade button that costs 10 clicks
-        - AutoClick upgrade is disabled otherwise
-        - AutoClick rate increases by 1 with each upgrade
+- !!STEP 6 COMPLETE!!
+- Added Clicks / sec counter
+- Added seperate Total Clicks counter
+- AutoClick Updates
+    - AutoClick is now seperated into 3 categories
+        - 0.1 Clicks Per Second costing 10 Clicks
+        - 2 Clicks Per Second costing 100 Clicks
+        - 50 Clicks Per Second costing 1000 Clicks
+    - Each individual AutoClick upgrade displays how many you posses and their growth rate
+- Fixed a typo that was consitently in the README
--- src/main.ts
+++ src/main.ts
@@ -11,28 +11,92 @@ header.innerHTML = gameName;
 app.append(header);
 
 //MAIN BUTTON TO CLICK
-const theGameButton = document.createElement("theGameButton");
-theGameButton.innerHTML = "🔥";
+const theGameButton = document.createElement("button");
+theGameButton.innerHTML = "CLICK MEEEHHHH";
+theGameButton.style.position = "absolute";
+theGameButton.style.left = "550px";
 
 let clicks = 0;
 let clickIncrease = 0;
-theGameButton.onclick = () => {     //INCREASE CLICKS ON BUTTON CLICK
+theGameButton.onclick = () => {
+  //INCREASE CLICKS ON BUTTON CLICK
   clicks++;
 };
 app.append(theGameButton);
 
+//AUTOCLICK UPGRADE LEVELS
 
-//AUTOCLICK UPGRADE
-const autoclickUpgrade = document.createElement("button");
-autoclickUpgrade.innerHTML = "Enable Autoclick Per Second";
 
-autoclickUpgrade.onclick = () => {
-  clickIncrease++;
-  clicks-=10;
-  theGameButton.innerHTML = `(${clicks | 0}) Clicks`;
-  autoclickUpgrade.innerHTML = `(${clickIncrease | 0}) Clicks Per Second; Click To Upgrade`;
+//LEVEL A//
+
+
+let upgradeLevels = [0,0,0];
+
+const autoclickUpgradeA = document.createElement("button");
+autoclickUpgradeA.innerHTML = "Enable 0.1 Autoclick Per Second";
+
+autoclickUpgradeA.onclick = () => {
+  clickIncrease+=0.1;
+  clicks -= 10;
+  upgradeLevels[0]++;
+  autoclickUpgradeA.innerHTML = `(${upgradeLevels[0]}) 1/10th Click Per Second; Click To Upgrade`;
 };
-app.append(autoclickUpgrade);
+
+autoclickUpgradeA.style.position = "absolute";
+autoclickUpgradeA.style.left = "20px";
+autoclickUpgradeA.style.top = "50px";
+app.append(autoclickUpgradeA);
+
+//LEVEL B//
+const autoclickUpgradeB = document.createElement("button");
+autoclickUpgradeB.innerHTML = "Enable 2 Autoclick Per Second";
+
+autoclickUpgradeB.onclick = () => {
+  clickIncrease+=2;
+  clicks -= 100;
+  upgradeLevels[1]++;
+  autoclickUpgradeB.innerHTML = `(${upgradeLevels[1]}) Double Clicks Per Second; Click To Upgrade`;
+};
+
+autoclickUpgradeB.style.position = "absolute";
+autoclickUpgradeB.style.left = "20px";
+autoclickUpgradeB.style.top = "100px";
+app.append(autoclickUpgradeB);
+
+//LEVEL C//
+const autoclickUpgradeC = document.createElement("button");
+autoclickUpgradeC.innerHTML = "Enable 50 Autoclick Per Second";
+
+autoclickUpgradeC.onclick = () => {
+  clickIncrease+=50;
+  clicks -= 1000;
+  upgradeLevels[2]++;
+  autoclickUpgradeC.innerHTML = `(${upgradeLevels[2]}) Triple Clicks Per Second; Click To Upgrade`;
+};
+
+autoclickUpgradeC.style.position = "absolute";
+autoclickUpgradeC.style.left = "20px";
+autoclickUpgradeC.style.top = "150px";
+app.append(autoclickUpgradeC);
+
+//DISPLAY CLICKS
+//🔥
+const displayClicks = document.createElement("p");
+displayClicks.style.position = "absolute";
+displayClicks.innerHTML = "im shocked if you manage to see this in-game";
+displayClicks.style.left = "970px";
+displayClicks.style.top = "50px";
+displayClicks.style.fontSize = "40px"
+app.append(displayClicks);
+
+//CPS
+const CPS = document.createElement("p");
+CPS.style.position = "absolute";
+CPS.innerHTML = "im shocked if you manage to see this in-game";
+CPS.style.left = "975px";
+CPS.style.top = "130px";
+CPS.style.fontSize = "15px"
+app.append(CPS);
 
 
 
@@ -43,11 +107,20 @@ function globalUpdate(FR: number): void {
   const dFR = (FR - lastTick) / 1000; //FRAMERATE CALC
   lastTick = FR;
 
-  amount = clickIncrease * dFR;
+  amount = clickIncrease * dFR;//AUTOCLICK FUNCT
   clicks = clicks + amount;
-  theGameButton.innerHTML = `(${clicks | 0}) Clicks`; //AUTOCLICK FUNCT
-  if (clicks <10) {autoclickUpgrade.disabled = true} else {autoclickUpgrade.disabled = false}; //IF AUTOCLICK UPGRADE 
-                                                                                              //IS AVAILABLE
+
+  displayClicks.innerHTML = `(${clicks | 0}) Clicks`; 
+  CPS.innerHTML = `(${((0.1 * upgradeLevels[0]) + (2 * upgradeLevels[1]) + (50 * upgradeLevels[2])) }) Clicks / Second`; 
+
+  //IF AUTOCLICK UPGRADE
+  //IS AVAILABLE
+  if (clicks < 10) {autoclickUpgradeA.disabled = true;} else {autoclickUpgradeA.disabled = false;}
+
+  if (clicks < 100) {autoclickUpgradeB.disabled = true;} else {autoclickUpgradeB.disabled = false;}
+
+  if (clicks < 1000) {autoclickUpgradeC.disabled = true;} else {autoclickUpgradeC.disabled = false;}
+
   requestAnimationFrame(globalUpdate);
 }
 requestAnimationFrame(globalUpdate);
