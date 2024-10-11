46be866c4838adb76dba407ab855b4c3ea046f77
AntonioPereda <anapered@ucsc.edu>
2024-10-11 16:16:51 -0700

    STEP 10 COMPLETE. READ README FOR FINAL NOTES

--- README.md
+++ README.md
@@ -2,5 +2,17 @@ This is the starter code project for the `Demo 1` project in CMPM 121, Game Desi
 
 ####CHANGELOG####
 
-- !!STEP 9 COMPLETE!!
-- Implimented Data-Driven Design to make future additions to Upgrades easier
+- !!STEP 10 COMPLETE! D1 FINISHED!!
+- Multiplicative Growth added
+    - Starts off at 1.0 (normal rate, no upgrades)
+    - Ashes Per Second get multipled by the Multiplicave rate before they get added to your total Ashes
+- Added 2 New Items to the Upgrade Shop
+    - Flamethrower
+        - Costs 5000 Ashes
+        - 4% Multiplicative Growth / Increases Multiplicave Growth Rate by .04 (additive)
+    - Smokey The Bear
+        - Costs 19440 Ashes
+        - 75% Multiplicative Growth / Increases Multiplicave Growth Rate by .75 (additive)
+        - Kidnapping Smokey The Bear also corrupts your moral compas and conscious
+
+- The dev is now going to play countless hours of SPARKING ZERO 
--- src/main.ts
+++ src/main.ts
@@ -21,8 +21,9 @@ theGameButton.style.fontSize = "140px";
 theGameButton.style.background = "green";
 theGameButton.style.border = "black";
 
-let clicks = 0;
+let clicks = 100000;
 let clickIncrease = 0;
+let multiplier = 1;
 theGameButton.onclick = () => {
   //INCREASE CLICKS ON BUTTON CLICK
   clicks++;
@@ -34,64 +35,105 @@ app.append(theGameButton);
 const upgradeLevels = [0, 0, 0];
 
 interface Item {
-  name: string,
-  cost: number,
-  rate: number,
-  left: string,
-  top: string,
-  hoverLeft: string,
-  hoverTop: string,
-  desc: string
-};
-
-const itemShop : Item[] = [
-
-  {name: "FirePit", 
-  cost: 10, rate: 0.1, 
-  left: "20px", 
-  top: "50px",
-  hoverLeft: "290px", 
-  hoverTop: "32px",
-  desc: "A cozy firepit, great for some camping.<br>Gain an additional 0.1 Ashes Per Second."
-  },
-  {name: "Charcoal", 
-  cost: 100, 
-  rate: 2, 
-  left: "20px", 
-  top: "100px",
-  hoverLeft: "300px", 
-  hoverTop: "80px",
-  desc: "Quality Charcoal to fuel the fire.<br>Gain an additional 2 Ashes Per Second."
-},
-  {name: "Lighter Fluid", 
-  cost: 1000, 
-  rate: 50, 
-  left: "20px", 
-  top: "150px",
-  hoverLeft: "335px", 
-  hoverTop: "130px",
-  desc: "Please dont set things on fire that shouldn't...<br>Gain an additional 50 Ashes Per Second."
+  name: string;
+  cost: number;
+  rate: number;
+  multrate: number;
+  left: string;
+  top: string;
+  hoverLeft: string;
+  hoverTop: string;
+  desc: string;
+  lvl: number;
 }
 
+const itemShop: Item[] = [
+  {
+    name: "FirePit",
+    cost: 10,
+    rate: 0.1,
+    multrate: 0,
+    left: "20px",
+    top: "50px",
+    hoverLeft: "290px",
+    hoverTop: "32px",
+    desc: "A cozy firepit, great for some camping.<br>Gain an additional 0.1 Ashes Per Second.",
+    lvl: 0
+  },
+  {
+    name: "Charcoal",
+    cost: 100,
+    rate: 2,
+    multrate: 0,
+    left: "20px",
+    top: "100px",
+    hoverLeft: "300px",
+    hoverTop: "80px",
+    desc: "Quality Charcoal to fuel the fire.<br>Gain an additional 2 Ashes Per Second.",
+    lvl: 0
+  },
+  {
+    name: "Lighter Fluid",
+    cost: 1000,
+    rate: 50,
+    multrate: 0,
+    left: "20px",
+    top: "150px",
+    hoverLeft: "335px",
+    hoverTop: "130px",
+    desc: "Please dont set things on fire that shouldn't...<br>Gain an additional 50 Ashes Per Second.",
+    lvl: 0
+  },
+  {
+    name: "Flamethrower",
+    cost: 5000,
+    rate: 0,
+    multrate: 0.04,
+    left: "20px",
+    top: "200px",
+    hoverLeft: "355px",
+    hoverTop: "180px",
+    desc: "Does this even have a practical everyday use?<br>Ashes Per Second Increases by 4%.",
+    lvl: 0
+  },
+  {
+    name: "Smokey The Bear",
+    cost: 19440,
+    rate: 0,
+    multrate: 0.75,
+    left: "20px",
+    top: "250px",
+    hoverLeft: "40px",
+    hoverTop: "280px",
+    desc: "WHAT ARE YOU DOING?????<br>Ashes Per Second Increases by 75%",
+    lvl: 0
+  }
+  
 ];
 
-
 //!!!CREATING BUTTONS VIA DDD!!!//
-let listOfButtons: HTMLButtonElement[] = [];
-for (let a = 0; a <= 2; a++){
-  let button = document.createElement("button");
-  if (upgradeLevels[a] == 0) {
+const listOfButtons: HTMLButtonElement[] = [];
+for (let a = 0; a <= itemShop.length-1; a++) {
+  console.log(a);
+  const button = document.createElement("button");
+  if (itemShop[a].lvl == 0) {
     if (a == 2 || a == 1) {
-      button.innerHTML = `Get ${itemShop[a].name} For: ${itemShop[a].cost | 0}  Ashes `;
-    } else {button.innerHTML = `Get a ${itemShop[a].name} For: ${itemShop[a].cost | 0}  Ashes `;}
+      button.innerHTML = `Get ${itemShop[a].name} For: ${itemShop[a].cost | 0}  Ashes`;
+    } else if (a == 4){
+      button.innerHTML = `Hold ${itemShop[a].name} Hostage For: ${itemShop[a].cost | 0}  Ashes`;
+    } else {
+      console.log(itemShop[a].name);
+      button.innerHTML = `Get a ${itemShop[a].name} For: ${itemShop[a].cost | 0}  Ashes`;
+    }
   }
 
   button.onclick = () => {
     clickIncrease += itemShop[a].rate;
+    multiplier += itemShop[a].multrate;
     clicks -= itemShop[a].cost;
-    upgradeLevels[a]++;
+    itemShop[a].lvl++;
     itemShop[a].cost *= 1.15;
-    button.innerHTML = `${itemShop[a].cost | 0} | ${upgradeLevels[a]} ${itemShop[a].name}`;
+    button.innerHTML = `${itemShop[a].cost | 0} | ${itemShop[a].lvl} ${itemShop[a].name}`;
   };
 
   button.style.position = "absolute";
@@ -100,10 +142,9 @@ for (let a = 0; a <= 2; a++){
   document.body.appendChild(button);
   listOfButtons.push(button);
 
-
   //HOVER SHOWS BONUS DETAILS
   button.addEventListener("mouseenter", function () {
-    let bonusText = document.createElement("p");
+    const bonusText = document.createElement("p");
     bonusText.style.left = itemShop[a].hoverLeft;
     bonusText.style.top = itemShop[a].hoverTop;
     bonusText.innerHTML = itemShop[a].desc;
@@ -120,10 +161,7 @@ for (let a = 0; a <= 2; a++){
   } else {
     button.disabled = false;
   }
-
 }
-
-
 
 //DISPLAY CLICKS 🗣️🗣️🗣️🔥🔥🔥
 const displayClicks = document.createElement("p");
@@ -144,16 +182,16 @@ CPS.style.fontSize = "15px";
 app.append(CPS);
 
 //IF AUTOCLICK UPGRADE IS AVAILABLE
-  //HELPER FUNCTION
+//HELPER FUNCTION
 
-  function checkAvailable(a: number, button: HTMLButtonElement){
-    if (clicks < itemShop[a].cost) {
-      button.disabled = true;
-    } else {
-      button.disabled = false;
-    }
+function checkAvailable(a: number, button: HTMLButtonElement) {
+  if (clicks < itemShop[a].cost) {
+    button.disabled = true;
+  } else {
+    button.disabled = false;
   }
-  
+}
+
 //GLOBAL UPDATE
 let lastTick = 0;
 let amount = clickIncrease;
@@ -161,17 +199,33 @@ function globalUpdate(FR: number): void {
   const dFR = (FR - lastTick) / 1000; //FRAMERATE CALC
   lastTick = FR;
 
-  amount = clickIncrease * dFR; //AUTOCLICK FUNCT
+  amount = (clickIncrease*multiplier) * dFR; //AUTOCLICK FUNCT
   clicks = clicks + amount;
 
   displayClicks.innerHTML = `(${clicks | 0}) Ashes`;
-  CPS.innerHTML = `(${0.1 * upgradeLevels[0] + 2 * upgradeLevels[1] + 50 * upgradeLevels[2]}) Ashes Per Second`;
 
-  for (let a = 0; a < listOfButtons.length; a++){
+  let totalCPS = 0;
+
+  for (let c = 0; c<itemShop.length; c++){
+    totalCPS += itemShop[c].lvl * itemShop[c].rate
+  }
+
+  if (multiplier == 1){
+    CPS.innerHTML = `${totalCPS} Ashes Per Second`;
+  } else {CPS.innerHTML = `${totalCPS}(X ${multiplier}) Ashes Per Second`; }
+  
+
+  for (let a = 0; a < listOfButtons.length; a++) {
     checkAvailable(a, listOfButtons[a]);
   }
-  
 
   requestAnimationFrame(globalUpdate);
 }
 requestAnimationFrame(globalUpdate);
+
+
+
+
+
+
+//SAIYAN LIMIT = "DNE"
